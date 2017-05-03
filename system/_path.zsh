4a00270c200aca675bcbc87fb5982c84fe7ea758
Vincent Palancher <vincent.palancher@gmail.com>
2017-05-03 12:46:21 +0200

    Refactor vimrc for neovim, add a git alias, add /home/vincent.palancher/.local/bin to PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH:$HOME/.local/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
