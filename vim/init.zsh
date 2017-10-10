23f1213cfc612b6890f8873b0cab995ac0252a99
Vincent PALANCHER <vincent.palancher@gmail.com>
2017-10-10 13:33:10 +0200

    Cleanup vim plugins

--- vim/init.zsh
+++
@@ -1,6 +0,0 @@
-bundledir=$HOME/.vim/bundle
-vundledir=$bundledir/Vundle.vim
-if ! test -e $vundledir; then
-    mkdir -p $bundledir
-    git clone https://github.com/VundleVim/Vundle.vim.git $vundledir
-fi
