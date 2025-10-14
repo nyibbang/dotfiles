dda7bf3e127fa75e3ea6d1d2faf07ccc2bba5319
Vincent PALANCHER <vincent.palancher@gmail.com>
2025-10-14 15:22:00 +0200

    update neovim config

--- neovim/nvim.symlink/lua/config/keymaps.lua
+++ neovim/nvim.symlink/lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 
 local map = vim.keymap.set
 
-map("x", "<leader>p", '"_dP', { desc = "Cut to blackhole then paste before cursor" })
-map({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to clipboard" })
-map("n", "<leader>Y", '"+Y', { desc = "Copy line to clipboard" })
-map({ "n", "v" }, "<leader>d", '"_d', { desc = "Cut to blackhole" })
-
 map("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next problem (quickfix)" })
 map("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Previous problem (quickfix)" })
 map("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next problem (location list)" })
--- neovim/nvim.symlink/lua/plugins/rust.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^5",
-    lazy = false, -- This plugin is already lazy
-  },
-}
